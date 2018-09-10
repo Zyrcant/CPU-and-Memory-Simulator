d2e04ed3756b7a662d45fad2fd4161d78abdfc02
Tiffany Diem Do <tdd160030@cslinux2.utdallas.edu>
2018-09-09 21:39:02 -0500

    Added more instruction sets, fixed more buffer issues.

--- Project1.cpp
+++ Project1.cpp
@@ -8,6 +8,9 @@
 #include <sys/wait.h>
 #include <vector>
 #include <sstream>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
 
 //helper functions to create instructions
 void createInstruction(char* a, char type, int pc);
@@ -56,9 +59,6 @@ int main(int argc, char **argv)
     close(pipes[1]);
     close(pipes2[0]);
 
-    mem[500] = 100;
-    mem[100] = 2;
-
     //process read/write requests from cpu
     while(true)
     {
@@ -86,7 +86,7 @@ int main(int argc, char **argv)
 	std::string location = elems[2];
 	std::cout<< "Writing" << toWrite << " to " << location << std::endl;
 	mem[std::stoi(location)] = std::stoi(toWrite);
-	std::cout<<mem[std::stoi(location)];
+	std::cout<<mem[std::stoi(location)]<<std::endl;
 	write(pipes2[1], "good", 5);
       }
       else if(elems[0].compare("e") == 0)
@@ -117,16 +117,14 @@ int main(int argc, char **argv)
   int y = 0;
   int operand;
 
+  //seeds time for random function (Instruction 8)
+  srand(time(NULL));
 
+  //stores the instructions
   char instruc[30];
 
   //buffer to confirm if writing has happened
   char confirm[30];
-
-  /*
-  createWrite(instruc, 'w', 300, 1999);
-  write(pipes[1], instruc, sizeof(instruc));
-  read(pipes2[0], &confirm, sizeof(confirm));*/
 
   //start CPU by requesting first instruction from memory
   createInstruction(instruc, 'r', pc);
@@ -147,24 +145,71 @@ int main(int argc, char **argv)
       case 2: //load value from the address found into AC
 	pc++; 
 	createInstruction(instruc, 'r', pc);
-	write(pipes[1], instruc, 31); //request address
+	write(pipes[1], instruc, sizeof(instruc)); //request address
 	read(pipes2[0], &operand, sizeof(operand)); //get address
 	createInstruction(instruc, 'r', operand); 
-	write(pipes[1], instruc, 31); //request what is at the address
+	write(pipes[1], instruc, sizeof(instruc)); //request what is at the address
 	read(pipes2[0], &ac, sizeof(ac));
 	break;
-     /* case 3: //load value from the address found in the given address into the AC
+      case 3: //load value from the address found in the given address into the AC
 	pc++; 
 	createInstruction(instruc, 'r', pc);
-	write(pipes[1], instruc, 31); //request first address
+	write(pipes[1], instruc, sizeof(instruc)); //request first address
 	read(pipes2[0], &operand, sizeof(operand)); //get first address
 	createInstruction(instruc, 'r', operand);
-	write(pipes[1], instruc, 31); //request second address
+	write(pipes[1], instruc, sizeof(instruc)); //request second address
 	read(pipes2[0], &operand, sizeof(operand)); //get second address;
 	createInstruction(instruc, 'r', operand); //request what is at the second address
-	write(pipes[1], instruc, 31);
+	write(pipes[1], instruc, sizeof(instruc));
 	read(pipes2[0], &ac, sizeof(ac));
-	break;*/
+	break;
+      case 4: //load value at address+X into the AC
+	pc++;
+	createInstruction(instruc, 'r', pc);
+	write(pipes[1], instruc, sizeof(instruc)); //request address
+	read(pipes2[0], &operand, sizeof(operand)); //get address
+	createInstruction(instruc, 'r', operand+x); //request what is at address+x
+	write(pipes[1], instruc, sizeof(instruc));
+	read(pipes2[0], &ac, sizeof(ac));
+	break;
+      case 5: //load value at address+Y into the AC
+	pc++;
+	createInstruction(instruc, 'r', pc);
+	write(pipes[1], instruc, sizeof(instruc)); //request address
+	read(pipes2[0], &operand, sizeof(operand)); //get address
+	createInstruction(instruc, 'r', operand+y); //request what is at address+x
+	write(pipes[1], instruc, sizeof(instruc));
+	read(pipes2[0], &ac, sizeof(ac));
+	break;
+      case 6: //load from sp+x into the AC
+	operand = sp + x;
+	createInstruction(instruc, 'r', operand);
+	write(pipes[1], instruc, sizeof(instruc));
+	read(pipes2[0], &ac, sizeof(ac));
+	break;
+      case 7: //store value in the AC into the address
+	pc++;
+	createInstruction(instruc, 'r', pc); 
+	write(pipes[1], instruc, sizeof(instruc)); //request address
+	read(pipes2[0], &operand, sizeof(operand));
+	createWrite(instruc, 'w', ac, operand);
+	write(pipes[1], instruc, sizeof(instruc)); 
+	read(pipes2[0], &confirm, sizeof(confirm)); //wait for memory to confirm write
+	break;
+      case 8: //get random int from 1-100 into the AC
+	ac = rand() % 100 + 1;
+	break;
+      case 9: //put port, if port = 1, writes AC as int to screen, if port = 2, writes AC as char
+	pc++;
+	createInstruction(instruc, 'r', pc);
+	write(pipes[1], instruc, sizeof(instruc)); 
+	read(pipes2[0], &operand, sizeof(operand));
+	c = ac;
+	if(operand == 1)
+	  std::cout << ac; 
+	else if (operand == 2)
+	  std::cout << static_cast<char>(ac);
+	break;
       default: 
 	perror("Not a valid command");
 	break;
@@ -203,7 +248,6 @@ void createWrite(char* a, char type, int pc, int location)
   std::string s = std::string() + type;
   std::string ins = s + " " + std::to_string(pc) + " " + std::to_string(location);
   strncpy(a, ins.c_str(), 30);
-  std::cout<<ins.length(); 
   a[ins.length()] = 0;
 }
 
